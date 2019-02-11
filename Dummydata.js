838bff90ad0ef7f8e602492647e41434f8ff498c
bastixx <bas_vdwaal@hotmail.com>
2019-02-11 10:48:11 +0100

    Initial commit

---
+++ Dummydata.js
@@ -0,0 +1,9 @@
+var data = {
+    "id1": {"user": "user1", "icon": "icon1", "location": "Thuis"},
+    "id3": {"user": "user3", "icon": "icon3", "location": "Thuis"},
+    "id2": {"user": "user2", "icon": "icon2", "location": "School"},
+    "id4": {"user": "user4", "icon": "icon4", "location": "Werk"}
+};
+
+// var locations = ["School", "Werk", "Thuis", "Tandarts", "Ziekenhuis"];
+//var locations = ["School", "Werk", "Thuis"];
