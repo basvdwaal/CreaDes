f9ea5759d7047de6d1116e3fc3031810fee45e7e
basvdwaal <bas_vdwaal@hotmail.com>
2019-02-22 16:58:26 +0100

    Rewrote hiding function, small changes

--- Dummydata.js
+++ Dummydata.js
@@ -1,7 +1,7 @@
 var data = {
-    "id1": {"user": "user1", "icon": "icon1", "location": "Thuis"},
-    "id3": {"user": "user3", "icon": "icon3", "location": "Thuis"},
-    "id2": {"user": "user2", "icon": "icon2", "location": "School"},
-    "id4": {"user": "user4", "icon": "icon4", "location": "Werk"},
-    "id5": {"user": "user5", "icon": "icon5", "location": "Ziekenhuis"}
+    "id1": {"user": "user1", "icon": "icon1", "location": "Thuis", "direction": "East"},
+    "id3": {"user": "user3", "icon": "icon3", "location": "Thuis", "direction": "Center"},
+    "id2": {"user": "user2", "icon": "icon2", "location": "School", "direction": "East"},
+    "id4": {"user": "user4", "icon": "icon4", "location": "Locatie 6", "direction": "West"},
+    "id5": {"user": "user5", "icon": "icon5", "location": "Ziekenhuis", "direction": "North"}
 };
