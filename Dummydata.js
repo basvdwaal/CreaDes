7734b5597c8f2085b1f6fdb30deae62bd50aea4a
bastixx <bastixx669@gmail.com>
2019-02-22 11:07:45 +0100

    Cleanup, added function to remove unused rows

--- Dummydata.js
+++ Dummydata.js
@@ -2,5 +2,6 @@ var data = {
     "id1": {"user": "user1", "icon": "icon1", "location": "Thuis"},
     "id3": {"user": "user3", "icon": "icon3", "location": "Thuis"},
     "id2": {"user": "user2", "icon": "icon2", "location": "School"},
-    "id4": {"user": "user4", "icon": "icon4", "location": "Werk"}
+    "id4": {"user": "user4", "icon": "icon4", "location": "Werk"},
+    "id5": {"user": "user5", "icon": "icon5", "location": "Ziekenhuis"}
 };
